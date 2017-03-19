1e99d002d3aa6d6df1799adb2280d82f8eca4ae2
jade <jade@Jade>
2017-03-19 17:11:09 +0100

    Aufgabe0

---
+++ Aufgabe0/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <script type="text/javascript">
+            function onload () {
+                document.getElementById("name").innerHTML = promtp("Name: ");
+            }
+        </script>
+    </head>
+    <body>
+        <p>Hallo <span id="name"></span></p>
+    </body>
+</html>
