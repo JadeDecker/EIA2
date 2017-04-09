0ad510560fcff47a5ae19118c5bfc8f03f5ae3ee
jade <jade@Jade>
2017-04-09 17:51:53 +0200

    Aufgabe3

---
+++ Aufgabe3/mau-mau/script.js
@@ -0,0 +1,73 @@
+var karten_zu = [
+    "Karo 7",
+    "Karo 8",
+    "Karo 9",
+    "Karo 10",
+    "Karo Bube",
+    "Karo Dame",
+    "Karo K&ouml;nig",
+    "Karo Ass",
+    "Pik 7",
+    "Pik 8",
+    "Pik 9",
+    "Pik 10",
+    "Pik Bube",
+    "Pik Dame",
+    "Pik K&ouml;nig",
+    "Pik Ass",
+    "Kreuz 7",
+    "Kreuz 8",
+    "Kreuz 9",
+    "Kreuz 10",
+    "Kreuz Bube",
+    "Kreuz Dame",
+    "Kreuz K&ouml;nig",
+    "Kreuz Ass",
+    "Herz 7",
+    "Herz 8",
+    "Herz 9",
+    "Herz 10",
+    "Herz Bube",
+    "Herz Dame",
+    "Herz K&ouml;nig",
+    "Herz Ass"
+];
+
+var hand = new Array;
+
+var karten_durch = new Array;
+
+function karte_ziehen () {
+    if (hand.length >= 5) {
+        alert("Du hast schon 5 Karten auf der Hand!");
+    } else {
+        var max = karten_zu.length;
+        var x = Math.round(Math.random() * max);
+        var text = karten_zu[x];
+        hand.push(text);
+        karten_zu.splice(x, 1);
+        zeigen();
+    }
+}
+
+function zeigen () {
+    document.getElementById("hand").innerHTML = "";
+    for (i = 0; i < hand.length; i++) {
+        document.getElementById("hand").innerHTML += '<div class="karte" onclick="ablegen('+i+');">'+hand[i].replace(" ", "<br>")+'</div>';
+    }
+    if (karten_zu.length == 0) {
+        document.getElementById("deck").style.display = "none";
+    }
+    if (karten_durch.length != 0) {
+        document.getElementById("deck2").style.display = "block";
+        var id = karten_durch.length-1;
+        document.getElementById("deck2").innerHTML = karten_durch[id].replace(" ", "<br>");
+    }
+}
+
+function ablegen (id) {
+    var text = hand[id];
+    karten_durch.push(text);
+    hand.splice(id, 1);
+    zeigen();
+}
---
+++ Aufgabe3/sissas/script.js
@@ -0,0 +1,55 @@
+function schachbrett () {
+    var n = 64;
+    var z = 1;
+    for (i = 1; i <= n; i++) {
+        var zeile = Math.floor((i-1) / 8)+1;
+        if (zeile%2 === 0) {
+            if (i%2 === 0) {
+                var background = "#000";
+                var color = "#fff";
+            } else {
+                var background = "#fff";
+                var color = "#000";
+            }
+        } else {
+            if (i%2 === 0) {
+                var background = "#fff";
+                var color = "#000";
+            } else {
+                var background = "#000";
+                var color = "#fff";
+            }
+        }
+        document.body.innerHTML += '<div id="' + i + '" style="background: ' + background + '; color: ' + color + '; border: 1px solid black;" onclick="markieren(' + i + ')">' + z + '</div>';
+        var z = z*2;
+    }
+}
+
+function markieren (id) {
+    var element = document.getElementById(id);
+    if(element.style.border == "1px solid black") {
+        element.style.border = "1px solid red";
+    } else {
+        element.style.border = "1px solid black";
+    }
+    rechnen();
+}
+
+function maus (e) {
+    document.getElementById("maus-div").style.left = e.clientX+20+"px";
+    document.getElementById("maus-div").style.top = e.clientY+20+"px";
+}
+
+function rechnen () {
+    var summe = 0;
+    var n = 64;
+    for (i = 1; i <= n; i++) {
+        var element = document.getElementById(i);
+        if(element.style.border == "1px solid red") {
+            var summe = summe + parseInt(element.innerHTML);
+        }
+    }
+    document.getElementById("maus-div").innerHTML = summe +"<br>"+summe.toString(16);;
+}
+
+document.onmousemove = maus;
